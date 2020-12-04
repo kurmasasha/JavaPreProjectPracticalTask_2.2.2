4feb9379caf09e127b79ee7bae3c8af4d0bcc721
Aleksandr Bro <drinkmaster85@mail.ru>
2020-12-05 02:59:00 +0300

    Java pre-project. Практическая задача 2.2.2

--- 2.2.2. spring_mvc/src/main/java/web/controller/HelloController.java
+++ 2.2.2. spring_mvc/src/main/java/web/controller/HelloController.java
@@ -11,12 +11,20 @@ import java.util.List;
 @Controller
 public class HelloController {
 
-	@GetMapping(value = "/")
+	@GetMapping(value = "/hello")
 	public String printWelcome(ModelMap model) {
 		List<String> messages = new ArrayList<>();
 		messages.add("Hello!");
 		messages.add("I'm Spring MVC application");
-		messages.add("5.2.0 version by sep'19 ");
+		messages.add("5.3.1 version by december'2020 ");
+		model.addAttribute("messages", messages);
+		return "index";
+	}
+
+	@GetMapping(value = "/bye")
+	public String printBye(ModelMap model) {
+		List<String> messages = new ArrayList<>();
+		messages.add("Bye-bye!");
 		model.addAttribute("messages", messages);
 		return "index";
 	}
